5befdfc3b2bbe985a7eae1ab40ce98464f879739
Christoffer M. Kramer <chris.m.kramer@outlook.dk>
2022-10-26 19:49:54 +0200

    small model

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from os import listdir
+from math import ceil
+
 
 
 ##############################
@@ -9,14 +14,13 @@ def generate_data(targ_size: tuple[int, int], batch_size: int) -> ImageDataGener
     """
     # ------ Training data ------
     train_datagen = ImageDataGenerator(
-        rescale=1./255,) # Scale between 0 and 1
-        #rotation_range=40, # Random rotations of 40 degree 
-        #width_shift_range=0.2, # Randomly Shift pixels between 0% and 20% in width
-        #height_shift_range=0.2, # Randomly shift pixels between 0% and 20% in height
-        #shear_range=0.2, # Shear angle in counter-clockwise direction in degrees
-        #zoom_range=0.2, # Zoom in between 0 and 20 %
-        #horizontal_flip=True,
-        #vertical_flip = True)
+        rescale=1./255) # Scale between 0 and 1
+        #rotation_range=0.05, # Random rotations of 10 degree 
+        #width_shift_range=0.05, # Randomly Shift pixels between 0% and 5% in width
+        #height_shift_range=0.05, # Randomly shift pixels between 0% and 5% in height
+        #shear_range=0.05, # Shear angle in counter-clockwise direction in degrees
+        #zoom_range=0.05, # Zoom in between 0 and 20 %
+        #horizontal_flip=True)
 
     train_generator = train_datagen.flow_from_directory(
         directory = r"catdog_data/train",
@@ -24,26 +28,27 @@ def generate_data(targ_size: tuple[int, int], batch_size: int) -> ImageDataGener
         color_mode = "rgb",
         batch_size = batch_size,
         class_mode='binary',
+        classes = ["cats","dogs"],
         shuffle = True) # Hypotese: Giver mere generaliserbar model, hvis shuffle er true
 
     # ------ Test data ------
     test_val_datagen = ImageDataGenerator(rescale=1./255) # Used for test and val, since we do not want to augment them
     test_generator = test_val_datagen.flow_from_directory(
-        directory="catdog_data/test",
+        directory= r"catdog_data/test",
         target_size = targ_size, # Optimal solution = 224x224 eller 112x112
         color_mode="rgb",
         batch_size = batch_size,
         class_mode= "binary",
-        shuffle = False,   
-    )
+        classes = ["cats","dogs"])
 
     # ------ Validation data ------
     val_generator = test_val_datagen.flow_from_directory( 
-        directory = "catdog_data/validation",
+        directory = r"catdog_data/validation",
         target_size = targ_size,
         color_mode = "rgb",
         batch_size= batch_size,
-        class_mode='binary')
+        class_mode='binary',
+        classes = ["cats","dogs"])
 
     # Full training data set
     full_train_generator = train_datagen.flow_from_directory(
@@ -52,6 +57,40 @@ def generate_data(targ_size: tuple[int, int], batch_size: int) -> ImageDataGener
         color_mode = "rgb",
         batch_size = batch_size,
         class_mode='binary',
+        classes = ["cats","dogs"],
         shuffle = True)
 
     return train_generator, val_generator, test_generator, full_train_generator
+
+
+def calc_steps_epoch(path_to_datadir: str = "catdog_data/train",
+                     batch_size: int = 32,
+                     factor: int | float = 1) -> int:
+    """
+    Return how many batches to go through in each epoch (is neccesarry since we use augmented data)
+    E.g. if there is 300 images in our directory and a batch size of 32, and we have it as a factor of 1
+    then we will go through celing( (1 * 300) / 32) = 10 batches
+    But we can raise the factor so we might end up going through more than the lenght of the directory (if there is little data)
+    """
+    return int(ceil( (factor * len(listdir(path_to_datadir)))/ batch_size)) 
+
+def plot_hist(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'b', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
