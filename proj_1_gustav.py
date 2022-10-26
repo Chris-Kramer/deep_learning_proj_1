9a941ca06a084bb3909e7df84a504cd7fbe4695a
Gustav Christensen <gustavchristensen95@gmail.com>
2022-10-26 21:38:44 +0200

    small_model

--- proj_1_gustav.py
+++ proj_1_gustav.py
@@ -1,151 +1,146 @@
-##############################
-# Part one - Data generators #
-##############################
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from os import listdir
-
-def generate_data(targ_size: tuple[int, int], batch_size: int) -> ImageDataGenerator:
-    """
-    Creates datagenerators with the specified target dimensions and batchsizes
-    """
-    # ------ Training data ------
-    train_datagen = ImageDataGenerator(
-        rescale=1./255, # Scale between 0 and 1
-        rotation_range=40, # Random rotations of 40 degree 
-        width_shift_range=0.2, # Randomly Shift pixels between 0% and 20% in width
-        height_shift_range=0.2, # Randomly shift pixels between 0% and 20% in height
-        shear_range=0.2, # Shear angle in counter-clockwise direction in degrees
-        zoom_range=0.2, # Zoom in between 0 and 20 %
-        horizontal_flip=True,
-        vertical_flip = True)
-
-    train_generator = train_datagen.flow_from_directory(
-        directory = "catdog_data/train",
-        target_size = targ_size,
-        color_mode = "rgb",
-        batch_size = batch_size,
-        class_mode='binary',
-        shuffle = True) # Hypotese: Giver mere generaliserbar model, hvis shuffle er true
-
-    # ------ Test data ------
-    test_val_datagen = ImageDataGenerator(rescale=1./255) # Used for test and val, since we do not want to augment them
-    test_generator = test_val_datagen.flow_from_directory(
-        directory="catdog_data/test",
-        target_size = targ_size, # Optimal solution = 224x224 eller 112x112
-        color_mode="rgb",
-        batch_size = batch_size,
-        class_mode= "binary",
-        shuffle = False,   
-    )
-
-    # ------ Validation data ------
-    val_generator = test_val_datagen.flow_from_directory( 
-        directory = "catdog_data/validation",
-        target_size = targ_size,
-        color_mode = "rgb",
-        batch_size= batch_size,
-        class_mode='binary')
-
-    # Full training data set
-    full_train_generator = train_datagen.flow_from_directory(
-        directory = "catdog_data/train_val",
-        target_size = targ_size,
-        color_mode = "rgb",
-        batch_size = batch_size,
-        class_mode='binary',
-        shuffle = True)
-
-    return train_generator, val_generator, test_generator, full_train_generator
-
-input_shape = (224, 224, 3)
-batch_size = 32
-train_generator, val_generator, test_generator, full_train_generator = generate_data(targ_size = (input_shape[0], input_shape[1]), batch_size = batch_size)
-
-# ------ Convnet ------
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
+from tensorflow.keras import regularizers as regu
 
+from utils import generate_data
+from utils import calc_steps_epoch
+from utils import plot_hist
+
+from os import listdir
+from math import ceil
+
+##############################
+# Part one - Data generators #
+##############################
+input_shape = (150, 150, 3)
+batch_size = 32
+train_generator, val_generator, test_generator, full_train_generator = generate_data(targ_size = (input_shape[0], input_shape[1]), batch_size = batch_size)
+
+####################
+# Part two - Model #
+####################
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',
-                        input_shape=input_shape))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+
+# Conv 1
+model.add(layers.Conv2D(filters = 32,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l2(0.0001),
+                        #bias_regularizer = regu.l2(0.0001),
+                        #activity_regularizer = regu.l2(0.0001),
+                        activation = 'relu',
+                        input_shape = input_shape))
+model.add(layers.MaxPooling2D(pool_size = 2))
+
+model.add(layers.Conv2D(filters = 64,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l2(0.0001),
+                        #bias_regularizer = regu.l2(0.0001),
+                        #activity_regularizer = regu.l2(0.0001),
+                        activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size = 2))
+
+model.add(layers.Conv2D(filters = 64,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l2(0.0001),
+                        #bias_regularizer = regu.l2(0.0001),
+                        #activity_regularizer = regu.l2(0.0001),
+                        activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size = 2))
+
 model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(248,
+                        kernel_regularizer = regu.l2(0.0001),
+                        #bias_regularizer = regu.l2(0.0001),
+                        #activity_regularizer = regu.l2(0.0001),
+                       activation='relu'))
+
+layers.Dropout(.2)
+
 model.add(layers.Dense(1, activation='sigmoid'))
 
-model.summary()
+model.compile(loss = 'binary_crossentropy',
+                      optimizer = optimizers.Adam(learning_rate = 0.001),
+                      metrics = ['accuracy'])
+"""
+model = models.Sequential()
 
-# ------ Configuration ------
-model.compile(
-    loss = "binary_crossentropy",
-    optimizer = optimizers.RMSprop(lr=1e-4),
-    metrics = ["accuracy"])
+# Conv 1
+model.add(layers.Conv2D(filters = 32,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l1_l2(l1=1e-5,l2=1e-4),
+                        bias_regularizer = regu.l2(1e-4),
+                        activity_regularizer = regu.l2(1e-5),
+                        activation = 'relu',
+                        input_shape = input_shape))
 
-# ------ Training the model ------
-from math import ceil
-def calc_steps_epoch(path_to_datadir: str = "catdog_data/train",
-                     batch_size: int = 32,
-                     factor: int | float = 1) -> int:
-    """
-    Return how many batches to go through in each epoch (is neccesarry since we use augmented data)
-    E.g. if there is 300 images in our directory and a batch size of 32, and we have it as a factor of 1
-    then we will go through celing( (1 * 300) / 32) = 10 batches
-    But we can raise the factor so we might end up going through more than the lenght of the directory (if there is little data)
-    """
-    return int(ceil( (factor * len(listdir(path_to_datadir)))/ batch_size)) 
 
+model.add(layers.MaxPooling2D(pool_size = 2))
+# Conv 1
+model.add(layers.Conv2D(filters = 64,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l1_l2(l1=1e-5,l2=1e-4),
+                        bias_regularizer = regu.l2(1e-4),
+                        activity_regularizer = regu.l2(1e-5),
+                        activation = 'relu'))
+
+
+# Max pool 1
+model.add(layers.MaxPooling2D(pool_size = 2))
+
+# COnv 2
+model.add(layers.Conv2D(filters = 64,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l1_l2(l1=1e-5,l2=1e-4),
+                        bias_regularizer = regu.l2(1e-4),
+                        activity_regularizer = regu.l2(1e-5),
+                        activation = 'relu'))
+
+
+model.add(layers.MaxPooling2D(pool_size = 2))
+
+# Conv 3
+model.add(layers.Conv2D(filters = 64,
+                        kernel_size = (3, 3),
+                        kernel_regularizer = regu.l1_l2(l1=1e-5,l2=1e-4),
+                        bias_regularizer = regu.l2(1e-4),
+                        activity_regularizer = regu.l2(1e-5),
+                        activation = 'relu'))
+
+
+model.add(layers.MaxPooling2D(pool_size = 2))
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(112,
+                        kernel_regularizer = regu.l1_l2(l1=1e-5,l2=1e-4),
+                        bias_regularizer = regu.l2(1e-4),
+                        activity_regularizer = regu.l2(1e-5),
+                       activation='relu'))
+
+layers.Dropout(.2)
+
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss = 'binary_crossentropy',
+                      optimizer = "Adam", #optimizers.Adam(learning_rate = 0.001),
+                      metrics = ['accuracy'])
+
+"""
+#callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=7)
 history = model.fit(
-      train_generator,
-      steps_per_epoch=50,
-      epochs=50,
-      validation_data=val_generator,
-      validation_steps=20,
-      verbose = True)
+    train_generator,
+    #steps_per_epoch =  calc_steps_epoch("catdog_data/train", batch_size), # How ma
+    batch_size=batch_size,
+    epochs = 100,
+    validation_data = val_generator,
+    #validation_steps = calc_steps_epoch("catdog_data/validation", batch_size),
+    verbose = True#,
+    #callbacks=[callback]
+)
 
-# ------ Loss and Accuracy ------
-import matplotlib.pyplot as plt
+model.evaluate(test_generator, steps=calc_steps_epoch("catdog_data/test", batch_size, 1))
 
-accuracy = history.history["accuracy"]
-val_accuracy = history.history["val_accuracy"]
-loss = history.history["loss"]
-val_loss = history.history["val_loss"]
-epochs = range(1, len(accuracy) + 1)
-plt.plot(epochs, accuracy, "bo", label="Training accuracy")
-plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
-plt.title("Training and validation accuracy")
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, "bo", label="Training loss")
-plt.plot(epochs, val_loss, "b", label="Validation loss")
-plt.title("Training and validation loss")
-plt.legend()
-plt.show()
 
-# ------ Evaluating model on test set ------
-loss, accuracy = model.evaluate(test_generator, steps=calc_steps_epoch("catdog_data/test", batch_size, 1))
-print(f"Test accuracy: {accuracy:.3f}")
-
-#################
-# Visualization #
-#################1
-
-# ------ Data Augmentation Example ------
-import tensorflow
-size = (300, 300)
-
-elephants = tensorflow.keras.utils.load_img("catdog_data/train/cats/cat.1.jpg", target_size=size)
-elephants = tensorflow.keras.utils.img_to_array(elephants)
-imshow(elephants)
-
-# ------ Filters ------
-
-# ------ Activation Maps ------
+plot_hist(history)
